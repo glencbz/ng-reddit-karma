1f9d6f85f84dc715aa006cf8f085551e823f6f93
glen <glencbz@gmail.com>
2016-11-24 11:00:59 +0800

    initial version with todo swapping!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  todos : string[] = [];
+
+  createTodo(newTodo: string) : void {
+    this.todos.push(newTodo);
+    console.log(this.todos);
+  }
+
+  deleteTodo(index: number) : void {
+    this.todos.splice(index, 1);
+  }
+
+  changeTodoRank(index: number, change: number): void{
+    var newPos = index + change;
+    if (newPos > this.todos.length || newPos < 0)
+      return;
+    var targeted : string = this.todos[index];
+    this.todos[index] = this.todos[newPos];
+    this.todos[newPos] = targeted;
+  }
 }
