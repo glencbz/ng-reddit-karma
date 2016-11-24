3f092a7e84ffb00972b21e87f2db938cfdac0919
glen <glencbz@gmail.com>
2016-11-24 11:16:29 +0800

    implemented up and down karma without sorting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,28 @@
 import { Component } from '@angular/core';
 
+class Todo {
+  public karma: number = 0;
+  changeKarma(change: number){
+    this.karma += change;
+  }
+
+  constructor(public content: string){}
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  todos : string[] = [];
 
-  createTodo(newTodo: string) : void {
-    this.todos.push(newTodo);
-    console.log(this.todos);
+export class AppComponent {
+  todos : Todo[] = [];
+
+  createTodo(content: string) : void {
+    this.todos.push(new Todo(content));
   }
 
   deleteTodo(index: number) : void {
     this.todos.splice(index, 1);
-  }
-
-  changeTodoRank(index: number, change: number): void{
-    var newPos = index + change;
-    if (newPos > this.todos.length || newPos < 0)
-      return;
-    var targeted : string = this.todos[index];
-    this.todos[index] = this.todos[newPos];
-    this.todos[newPos] = targeted;
   }
 }
