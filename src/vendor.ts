629cc7a6d535703405ba629c5b5a83f32137876d
glen <glencbz@gmail.com>
2016-11-29 15:19:46 +0800

    migrated from angular-cli to webpack

---
+++ src/vendor.ts
@@ -0,0 +1,13 @@
+// angular
+import "@angular/common";
+import "@angular/compiler";
+import "@angular/core";
+import "@angular/forms";
+import "@angular/http";
+import "@angular/platform-browser";
+import "@angular/platform-browser-dynamic";
+// lo-dash
+// import "@types/lodash";
+import "lodash";
+// rxjs
+import "rxjs";
